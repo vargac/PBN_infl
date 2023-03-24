18f078d2c9281a967c8f8b7ff74d2eef7467e1c7
Simon Varga <xvarga7@fi.muni.cz>
2023-03-24 13:00:15 +0100

    IBMFA averaging over parametrizations

--- pbn_ibmfa/src/main.rs
+++ pbn_ibmfa/src/main.rs
@@ -19,35 +19,77 @@ struct Fixing {
     value: bool,
 }
 
-fn ibmfa_step(update_functions: &[Bdd], probs: &[f32], init_probs: &[f32],
-              var_index: &HashMap<BddVariable, usize>) -> Vec<f32> {
-    update_functions
-        .iter()
+#[derive(Clone, Debug)]
+struct ParedUpdateFunction {
+    function: Bdd,
+    parametrizations: Bdd,
+}
+
+impl ParedUpdateFunction {
+    fn new(update_function: &Bdd, unit_bdd: &Bdd) -> ParedUpdateFunction {
+        let support_set = update_function.support_set();
+        let mut parametrizations = unit_bdd.clone();
+        for bdd_var in unit_bdd.support_set() {
+            if !support_set.contains(&bdd_var) {
+                parametrizations = parametrizations.var_project(bdd_var);
+            }
+        }
+        ParedUpdateFunction {
+            function: update_function.and(&parametrizations),
+            parametrizations,
+        }
+    }
+}
+
+type VarIndex = HashMap<BddVariable, usize>;
+
+fn clause_probability(
+    clause: &BddPartialValuation,
+    probs: &[f32],
+    var_index: &VarIndex)
+-> f32 {
+    clause.to_values().iter()
+        .map(|&(var, val)| {
+            let prob_one = probs[var_index[&var]];
+            if val { prob_one } else { 1.0 - prob_one }
+        })
+        .product()
+}
+
+fn ibmfa_step(
+    pupdate_functions: &[ParedUpdateFunction],
+    probs: &[f32],
+    init_probs: &[f32],
+    var_index: &HashMap<BddVariable, usize>)
+-> Vec<f32> {
+    pupdate_functions.iter()
         .enumerate()
-        .map(|(i, update_function)|
+        .map(|(i, pupdate_function)|
             if init_probs[i] == 0.0 || init_probs[i] == 1.0 {
                 init_probs[i]
             } else {
-                update_function
+                let mut pnumber = 0;
+                pupdate_function.parametrizations
                     .sat_clauses()
-                    .map(|clause|
-                        clause
-                            .to_values()
-                            .iter()
-                            .map(|&(var, val)| {
-                                let prob_one = probs[var_index[&var]];
-                                if val { prob_one } else { 1.0 - prob_one }
-                            })
-                            .product::<f32>())
-                    .sum()
+                    .map(|parametrization| {
+                        pnumber += 1;
+                        pupdate_function.function
+                            .restrict(&parametrization.to_values())
+                            .sat_clauses()
+                            .map(|clause|
+                                clause_probability(&clause, &probs, &var_index))
+                            .sum::<f32>()
+                    })
+ // TODO May be just count the number of parametrizations and iterate over all?
+                    .sum::<f32>() / pnumber as f32
             })
         .collect()
 }
 
-fn ibmfa(update_functions: &[Bdd], probs: &mut Vec<Vec<f32>>,
+fn ibmfa(pupdate_functions: &[ParedUpdateFunction], probs: &mut Vec<Vec<f32>>,
          var_index: &HashMap<BddVariable, usize>, verbose: bool) {
     for i in 1..probs.len() {
-        probs[i] = ibmfa_step(update_functions, &probs[i - 1], &probs[0],
+        probs[i] = ibmfa_step(&pupdate_functions, &probs[i - 1], &probs[0],
                               var_index);
         if verbose {
             println!("{:?}", probs[i]);
@@ -65,7 +107,7 @@ fn entropy(probs: &[f32]) -> f32 {
 /* early_stop is not a good idea. May be rather simulate until the values
  * converge, up to max iteration number. */
 fn ibmfa_entropy(
-    update_functions: &[Bdd],
+    pupdate_functions: &[ParedUpdateFunction],
     init_probs: &[f32],
     var_index: &HashMap<BddVariable, usize>,
     iterations: usize,
@@ -75,7 +117,7 @@ fn ibmfa_entropy(
     let mut probs: Vec<f32> = init_probs.into();
     let mut ent = 0.0;
     for _ in 0..iterations {
-        probs = ibmfa_step(update_functions, &probs, init_probs, var_index);
+        probs = ibmfa_step(&pupdate_functions, &probs, init_probs, var_index);
         if verbose {
             println!("{:?}", probs);
         }
@@ -127,7 +169,7 @@ fn main() {
 
     let context = SymbolicContext::new(&model).unwrap();
 
-    let mut bdd_var_index = HashMap::new();
+    let mut bdd_var_index: VarIndex = HashMap::new();
     for var_id in model.variables() {
         bdd_var_index
             .insert(context.get_state_variable(var_id), var_id.to_index());
@@ -151,30 +193,16 @@ fn main() {
     let unit_bdd = apply_regulation_constraints(context.mk_constant(true),
                                                 &model, &context).unwrap();
 
+    let pared_update_functions = update_functions.iter()
+        .map(|fun| ParedUpdateFunction::new(fun, &unit_bdd))
+        .collect::<Vec<_>>();
+
     for var_id in model.variables() {
-        let update_function = &update_functions[var_id.to_index()];
-        /*
-        println!("{}: {}",
-            model.get_variable_name(var_id),
-            update_function.to_boolean_expression(context.bdd_variable_set()));
-        for valuation in update_function.sat_clauses() {
-            println!("{}", valuation_to_str(&valuation, &context));
-        }
-        println!("AFTER UNIT_BDD");
-        */
-        let support_set = update_function.support_set();
-        let constrained_unit_bdd = unit_bdd
-            .project(context
-                .parameter_variables()
-                .iter()
-//                .chain(context.state_variables().iter())
-                .filter(|&bdd_variable| !support_set.contains(bdd_variable))
-                .copied()
-                .collect::<Vec<_>>()
-                .as_slice());
-        for parametrization in constrained_unit_bdd.sat_clauses() {
+        let pupdate_function = &pared_update_functions[var_id.to_index()];
+        for parametrization in pupdate_function.parametrizations.sat_clauses() {
             println!("{}", valuation_to_str(&parametrization, &context));
-            let f = update_function.restrict(&parametrization.to_values());
+            let f = pupdate_function.function
+                .restrict(&parametrization.to_values());
             println!("\t{}",
                 f.to_boolean_expression(context.bdd_variable_set()));
 
@@ -182,27 +210,17 @@ fn main() {
                 println!("\t{}", valuation_to_str(&valuation, &context));
             }
         }
-        /*
-        let constrained_update_function =
-            constrained_unit_bdd.and(update_function);
-        println!("{}", constrained_unit_bdd
-            .to_boolean_expression(context.bdd_variable_set()));
-        println!("{}", constrained_update_function
-            .to_boolean_expression(context.bdd_variable_set()));
-        for valuation in constrained_update_function.sat_clauses() {
-            println!("{}", valuation_to_str(&valuation, &context));
-        }
-        */
         println!();
     }
-    return;
+
 
     let iterations = 10;
     let mut probs = vec![vec![0 as f32; model.num_vars()]; iterations + 1];
     probs[0] = vec![0.5; model.num_vars()];
+//    probs[0][0] = 0.0;
 
     println!("Entropy: {}",
-        ibmfa_entropy(&update_functions, &probs[0], &bdd_var_index,
+        ibmfa_entropy(&pared_update_functions, &probs[0], &bdd_var_index,
                       iterations, true, true));
 
     let mut available_fixings = Vec::new();
@@ -219,15 +237,15 @@ fn main() {
             .map(|&fixing| {
                 let mut init_probs = vec![0.5; model.num_vars()];
                 init_probs[fixing.var_i] = if fixing.value { 1.0 } else { 0.0 };
-                let e = ibmfa_entropy(&update_functions, &init_probs,
-                                      &bdd_var_index, iterations, false, false);
-                println!("{}", e);
-                e
+                ibmfa_entropy(&pared_update_functions, &init_probs,
+                              &bdd_var_index, iterations, false, false)
             })
             .enumerate()
             .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
             .unwrap();
         let fixing = available_fixings.remove(min_entropy_index);
+        available_fixings.remove(min_entropy_index / 2 * 2);
+        println!("{fixing:?}, entropy: {min_entropy}");
         driver_set.push(fixing);
         if min_entropy == 0.0 {
             break;
