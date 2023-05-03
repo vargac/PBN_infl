c1c073fcb97a6a36350e5f67d3cd0c5388c2d2a7
Simon Varga <xvarga7@fi.muni.cz>
2023-05-03 15:10:50 +0200

    Move the license jsut to the copied code

--- pbn_ibmfa/src/symbolic_sync_graph/regulation_constraints.rs
+++ pbn_ibmfa/src/symbolic_sync_graph/regulation_constraints.rs
@@ -1,4 +1,27 @@
-// Copied from: github.com/sybila/biodivine-lib-param-bn/src/symbolic_async_graph/_impl_regulation_constraint.rs
+// Copy from: github.com/sybila/biodivine-lib-param-bn/src/symbolic_async_graph/_impl_regulation_constraint.rs
+//
+// MIT License
+//
+// Copyright (c) 2020 sybila
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 use biodivine_lib_param_bn::symbolic_async_graph::SymbolicContext;
 use biodivine_lib_param_bn::{BooleanNetwork, Monotonicity};
 use biodivine_lib_bdd::{bdd, Bdd};
