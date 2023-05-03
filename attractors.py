ffe958618c191e751289523ac87848baa8e9ace1
Simon Varga <xvarga7@fi.muni.cz>
2023-05-03 13:28:35 +0200

    Cleanup

--- attractors.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pyboolnet import file_exchange, state_transition_graphs, attractors
-
-primes = file_exchange.bnet2primes(sys.stdin.read())
-stg = state_transition_graphs.primes2stg(primes, 'asynchronous')
-steady, cyclic = attractors.compute_attractors_tarjan(stg)
-print(*primes.keys())
-print('steady', steady)
-print('cyclic', cyclic)
